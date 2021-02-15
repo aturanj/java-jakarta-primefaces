a48eefc5c554365cabdaa1a0e54b40c71f06fffd
a.turan <aturan@tuta.io>
2021-02-15 23:46:55 +0000

    added pagination, filtering and sorting

--- src/main/java/io/aturanj/sales/view/controllers/CustomerFacesController.java
+++ src/main/java/io/aturanj/sales/view/controllers/CustomerFacesController.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 import java.util.List;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
+import org.primefaces.model.FilterMeta;
 
 @Named(value = "customerFacesController")
 @SessionScoped
@@ -16,6 +17,9 @@ public class CustomerFacesController implements Serializable, IFacesController<C
     private ISalesService<Customer> customerService;
 
     private List<Customer> customers;
+    private List<Customer> customersFiltered;
+    private int customerCount;
+    private List<FilterMeta> filterBy;
 
     @Override
     public List<Customer> findAll() {
@@ -24,6 +28,27 @@ public class CustomerFacesController implements Serializable, IFacesController<C
 
     public List<Customer> getCustomers() {
         customers = findAll();
+        customerCount = customers.size();
         return customers;
+    }
+
+    public int getCustomerCount() {
+        return customerCount;
+    }
+
+    public List<Customer> getCustomersFiltered() {
+        return customersFiltered;
+    }
+
+    public void setCustomersFiltered(List<Customer> customersFiltered) {
+        this.customersFiltered = customersFiltered;
+    }
+
+    public List<FilterMeta> getFilterBy() {
+        return filterBy;
+    }
+
+    public void setFilterBy(List<FilterMeta> filterBy) {
+        this.filterBy = filterBy;
     }
 }
